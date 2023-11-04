66c06e1338a0b56d58a7a0cab52fba114cba67d0
Mitch Hollyfield <130614444+MiMeetch@users.noreply.github.com>
2023-11-04 10:20:57 -0400

    added firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyApzf5snsAOccmca3UFR25U1tuQcKv0QPk',
+  authDomain: 'lifeloom-4708b.firebaseapp.com',
+  projectId: 'lifeloom-4708b',
+  storageBucket: 'lifeloom-4708b.appspot.com',
+  messagingSenderId: '1016755450668',
+  appId: '1:1016755450668:web:cb4372997cec271a76b289',
+  measurementId: 'G-2V4BLQ1364',
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
